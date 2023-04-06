5b4e7289d343ad7d94708b7a57b50665fc16c11a
Ayagithubweb <ayaabdelmon@gmail.com>
2023-04-06 12:36:00 +0200

    🚩 Add Utilities to main

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,19 +1,27 @@
 import 'package:app/firebase_options.dart';
+import 'package:app/shared/components/constants.dart';
+import 'package:app/shared/cubit/bloc_observer.dart';
+import 'package:app/shared/cubit/cubit.dart';
 import 'package:app/shared/network/local/cache_helper.dart';
 import 'package:app/shared/network/remote/dio_helper.dart';
 import 'package:app/shared/router/router.dart';
 import 'package:app/shared/router/routes.dart';
+import 'package:app/shared/styles/theme.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   //ToDo:~> Add Firebase ------------------------------------------------------>
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
-  //ToDo:~> Add Services.... Dio, cache ------------------------------------------------------>
+  //ToDo:~> Add Services.... Dio, cache,Bloc --------------------------------------->
   DioHelper.init();
   await CacheHelper.init();
+  Bloc.observer = MyBlocObserver();
 
   runApp(const MyApp());
 }
@@ -23,16 +31,32 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+    //ToDo:~> Add BlocProvider ---------------------------------------------->
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(create: (_) => AppCubit()),
+      ],
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        debugShowCheckedModeBanner: false,
+        //ToDo: localizations ---------------------------------------------->
+        localizationsDelegates: const [
+                GlobalMaterialLocalizations.delegate,
+                GlobalWidgetsLocalizations.delegate,
+                GlobalCupertinoLocalizations.delegate,
+              ],
+              supportedLocales: [
+               lang? const Locale('ar', "AE"):const Locale('en'),
+              ],
+        //ToDo:~> Add Theme ----------------------------------------------------->
+        theme: lightTheme,
+        darkTheme: darkTheme,
+        themeMode: ThemeMode.light,
 
-      //ToDo:~> Add Routes -------------------------------------------------------->
-      onGenerateRoute: onGenerate,
-      initialRoute: AppRoutes.testRoute,
+        //ToDo:~> Add Routes ---------------------------------------------------->
+        onGenerateRoute: onGenerate,
+        initialRoute: AppRoutes.testRoute,
+      ),
     );
   }
 }
